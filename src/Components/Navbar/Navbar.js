9e62a9ab1989b08639547724ad88ddb3691e3f07
fabioFG <fabio.fernando.guerreiro@gmail.com>
2021-12-29 21:02:15 +0000

    add navbar responsive and dropdown toggle

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,28 @@
-import { Link } from "react-router-dom";
-import './Navbar.css';
+import "./Navbar.css";
+import { useState } from "react";
 
 function Navbar() {
-    return ( 
-        <nav>
-        FabioGuerreiro
-        <Link to="/">About me</Link>
-        <Link to="/">Projects</Link>
-        <Link to="/">Contacts</Link>
-        </nav>
-     );
+  //state for the navbar
+
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <nav>
+      <div className="left-side">FabioGuerreiro</div>
+      <div className="right-side">
+        <div className="links" id={showLinks ? "hidden" : ""}>
+          <a href="#about">About Me</a>
+          <a href="#projects">Projects</a>
+          <a href="#contacts">Contacts</a>
+        </div>
+        <button onClick={() => setShowLinks(!showLinks)} className="dropdown">
+          <div className="menu">
+            <div className="menu-hamburguer"></div>
+          </div>
+        </button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
