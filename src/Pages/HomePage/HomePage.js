b1ab7bb4350e668516a9981ecfb03a7b4f6f8895
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-01-05 00:56:43 +0000

    change text from about me section

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -88,18 +88,24 @@ function HomePage() {
           <div className="about-me-text">
             <p>
               {" "}
-              I am a Web Developer based in Lisbon, Portugal and I am also an
-              aspiring manga artist. I first started to deal with the world of
+              I am a Web Developer based in Lisbon, Portugal. I first started to deal with the world of
               tech in 2014 when I did a multimedia course in Lisbon. Although I
               did not pursue a career in the big world of tech at the time I was
               always keeping an eye on it and after completing a bachelor's and
               a master's degree in Translation I decided to completely change my
               life around.
             </p>
-            <br></br> In 2020 I started to dabble with HTML, CSS and JavaScript
-            and I became addicted to know more and learn how things worked. In
-            2021, I completed a Full Stack Web Development course and my goal is
-            is to combine technology with beautiful designs and aesthetics.{" "}
+            <br></br> In 2020 I started to play with HTML, CSS and JavaScript
+            and I became addicted on how things worked and on how I could learn more. After that, in
+            2021, I completed a Full Stack Web Development course focused on the MERN stack and my goal for the future is to combine technology with beautiful designs and aesthetics.
+            <br></br>
+            <br></br>
+            <p><b>My Hobbies</b></p>
+            <p> Drawing Manga art, playing online games and binge-watch anime and series whenever I can.</p>
+            <br></br>
+           
+            <p><b>What's next for me</b></p>
+            I plan on learning and studying more about Algorithms and Data Structure. Learn SASS, TypeScript and C#. 
           </div>
         </div>
       </article>
