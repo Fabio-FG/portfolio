cbf739d9e34b582c643417ba9fa5d57bb6fe0b3e
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-01-08 00:29:12 +0000

    add multiple fonts, aboutMe component and changes in navbar and footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,24 +1,48 @@
 import githubLogo from "../../images/github.svg";
 import linkedinLogo from "../../images/linkedin.svg";
 import gmailLogo from "../../images/gmail.svg";
-import './Footer.css';
-
-
+import "./Footer.css";
 
 function Footer() {
-    return ( 
-        <footer>
-            <h2>Contact Me</h2>
-        <div className="footer-container" id="contact-me">
-          <div className="footer-contacts">
-            <a href="https://github.com/Fabio-FG" target="_blank" rel="noreferrer"><span><img src={githubLogo} alt="github-logo" id="github-logo"/></span></a>
-            <a href="https://www.linkedin.com/in/fabiofguerreiro/" target="_blank" rel="noreferrer"><span><img src={linkedinLogo} alt="github-logo" id="github-logo"/></span></a>
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=someone@gmail.com" target="_blank" rel="noreferrer"><span><img src={gmailLogo} alt="github-logo" id="github-logo"/></span></a>
-          </div>
-          
+  return (
+    <footer>
+      <h2>Get in touch with me!</h2>
+      <div className="footer-container" id="contact-me">
+        <div className="footer-contacts">
+          <a
+            href="https://github.com/Fabio-FG"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>
+              <img src={githubLogo} alt="github-logo" id="github-logo" />
+            </span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/fabiofguerreiro/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>
+              <img src={linkedinLogo} alt="linkedin-logo" id="github-logo" />
+            </span>
+          </a>
+
+          <a
+            href="mailto:fabio.fernando.guerreiro@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span>
+              <img src={gmailLogo} alt="gmail-logo" id="github-logo" />
+            </span>
+          </a>
+          {/* auto start a gmail - to be determined */}
+          {/* <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=someone@gmail.com" target="_blank" rel="noreferrer"><span><img src={gmailLogo} alt="github-logo" id="github-logo"/></span></a> */}
         </div>
-        </footer>
-     );
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -15,6 +15,7 @@ import hybridboxImage from "../../images/hybridboxsite.png"
 import handlebarsLogo from "../../images/handlebars.svg";
 import ScrollToTopButton from "../../Components/ScrollToTopButton/ScrollToTopButton";
 import { useLayoutEffect } from "react";
+import AboutMeText from "../../Components/AboutMeText/AboutMeText";
 
 import Footer from "../../Components/Footer/Footer";
 
@@ -95,26 +96,7 @@ function HomePage() {
         <h2>About me</h2>
         <div className="about-me-wrapper">
           <div className="about-me-text">
-            <p>
-              {" "}
-              I am a Web Developer based in Lisbon, Portugal. I first started to deal with the world of
-              tech in 2014 when I did a multimedia course in Lisbon. Although I
-              did not pursue a career in the big world of tech at the time I was
-              always keeping an eye on it and after completing a bachelor's and
-              a master's degree in Translation I decided to completely change my
-              life around.
-            </p>
-            <br></br> In 2020 I started to play with HTML, CSS and JavaScript
-            and I became addicted on how things worked and on how I could learn more. After that, in
-            2021, I completed a Full Stack Web Development course focused on the MERN stack and my goal for the future is to combine technology with beautiful designs and aesthetics.
-            <br></br>
-            <br></br>
-            <p><b>My Hobbies</b></p>
-            <p> Drawing Manga art, playing online games and binge-watch anime and series whenever I can.</p>
-            <br></br>
-           
-            <p><b>What's next for me</b></p>
-            I plan on learning and studying more about Algorithms and Data Structure. Learn SASS, TypeScript and C#. 
+            <AboutMeText />
           </div>
         </div>
       </article>
